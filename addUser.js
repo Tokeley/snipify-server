6195217263e5d1136ee876b3bcf106e70136e7e5
Alex Manning <alex.t.manning@gmail.com>
2025-05-02 12:45:22 +1200

    headless: true

--- addUser.js
+++ addUser.js
@@ -22,7 +22,7 @@ async function sendFailureEmail(fullName, email) {
 
 export async function runAddUserScript(fullName, email) {
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
     slowMo: 100,
     chromiumSandbox: false
   });
